047e93fc8eeff0796b25c180e9f57a9518158f44
Prisca-Nova <148444234+Prisca-Nova@users.noreply.github.com>
2025-11-02 10:42:57 +0200

    other tasks  changes

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,7 @@
 INSERT INTO Customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (2, 'John Smith', 'jsmith@email.com', '456 Peace Street'),
+(3, 'Mary Johnson', 'mjohnson@email.com', '789 Joy Road'),
+(4, 'David Wilson', 'dwilson@email.com', '321 Hope Lane'),
+(5, 'Sarah Brown', 'sbrown@email.com', '654 Dream Drive');
